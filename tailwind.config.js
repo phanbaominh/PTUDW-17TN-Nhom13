f9230037722c92e09e9d7aad4d841613de8d7681
1712932 <abcdef6199@gmail.com>
2020-05-22 16:01:24 +0700

    Added responsive for header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   theme: {
     screens: {
-      "3xl": { max: "1919px" },
+      "4xl": { max: "1919px" },
+      "3xl": { max: "1659px" },
       "2xl": { max: "1439px" },
       xl: { max: "1279px" },
       lg: { max: "1023px" },
@@ -14,18 +15,26 @@ module.exports = {
         lato: ["Lato", "sans-serif"]
       },
       fontSize: {
+        "14": "1.4rem",
+        "16": "1.6rem",
+        "18": "1.8rem",
         "20": "2rem",
         "24": "2.4rem",
         "28": "2.8rem"
       },
       lineHeight: {
+        "18": "1.8rem",
+        "20": "2rem",
         "24": "2.4rem",
         "28": "2.8rem",
         "32": "3.2rem",
         "38": "3.8rem"
       },
       spacing: {
+        "p-38": "3.8rem",
+        "p-44": "4.4rem",
         "p-54": "5.4rem",
+        "p-70": "7rem",
         "p-85": "8.5rem"
       },
       colors: {
@@ -33,6 +42,7 @@ module.exports = {
         "fit-blue": "#131A22"
       },
       borderRadius: {
+        "6": "0.6rem",
         "10": "1rem"
       }
     }
