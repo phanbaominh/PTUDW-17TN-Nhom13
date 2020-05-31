148c9b3ee051055bd07a66f997899a4d4b156388
1712932 <abcdef6199@gmail.com>
2020-05-31 09:34:27 +0700

    Split header code

--- client/index.ts
+++ client/index.ts
@@ -1,22 +1,6 @@
 import $ from "jquery";
-
-function setHeaderSelectWidth(this: HTMLElement) {
-  let currentText = $(this).find(".main option:selected").text();
-  $(this).find(".tmp option").text(currentText);
-  $(this).find(".main").width($(this).find(".tmp").width()!);
-}
+import header from "./header";
 
 $(document).ready(function () {
-  $("#header-select").each(setHeaderSelectWidth);
-  $("#header-select").change(setHeaderSelectWidth);
-
-  // Handlers for sidebar
-  $("#header-hamburger").click(function () {
-    $("#sidebar").toggleClass("active");
-  });
-  $("body").click(function (e: JQuery.ClickEvent) {
-    if ($("#sidebar-wrapper").has(e.target).length === 0) {
-      $("#sidebar").removeClass("active");
-    }
-  });
+  header.setup();
 });
