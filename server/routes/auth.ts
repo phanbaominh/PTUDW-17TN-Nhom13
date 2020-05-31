2d3f5821478cc8292e6bb4e0231af0be0d580d73
1712932 <abcdef6199@gmail.com>
2020-05-31 17:12:45 +0700

    Implement placeholder for auth routes

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,10 +1,39 @@
-import express from "express";
+import express, { Request } from "express";
+import { requireAuth } from "../middlewares/auth";
+import { DUMMY_USER } from "../models/User";
 var router = express.Router();
 
 router.get("/login", function (req, res, next) {
+  if (res.locals.user) {
+    res.redirect("/");
+    return;
+  }
   res.render("login", {
     title: "Login"
   });
+});
+
+/**
+ * This route is just a placeholder for the actual login route (which will be implemented later)
+ * The only account allowed to login is (1712092, 123)
+ */
+router.post("/login", function (req: Request, res, next) {
+  let { username, password }: { username: string; password: string } = req.body;
+  if (username !== "1712092" || password !== "123") {
+    res.status(401).redirect("/login");
+    return;
+  }
+  res.cookie("authToken", JSON.stringify(DUMMY_USER));
+  res.redirect("/");
+});
+
+router.get("/logout", function (req, res, next) {
+  res.clearCookie("authToken");
+  res.redirect("/");
+});
+
+router.get("/profile", requireAuth, function (req, res, next) {
+  res.send("Hello there");
 });
 
 router.get("/forgot-password", function (req, res, next) {
--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,7 @@ import hbs from "hbs";
 
 import indexRouter from "./routes/index";
 import authRouter from "./routes/auth";
+import { parseAuth } from "./middlewares/auth";
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", "public")));
+app.use("*", parseAuth);
 
 app.use("/", indexRouter);
 app.use("/", authRouter);
