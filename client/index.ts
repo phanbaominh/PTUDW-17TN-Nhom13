2f1e2f65ecea4c7fda550d644673728ade508688
Minh Phan <pbminh2511+PTUDW@gmail.com>
2020-05-31 11:29:22 +0700

    Finish footer and add font-awesome pkg

--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,7 @@
 import $ from "jquery";
 import header from "./header";
-
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
 $(document).ready(function () {
   header.setup();
 });
