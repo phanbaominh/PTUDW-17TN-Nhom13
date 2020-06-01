f9145f2f49996c1828dccb14d122f35ea9dc2517
1712092 <pbminh2511+PTUDW@gmail.com>
2020-06-01 21:25:46 +0700

    Add responsive

--- server/routes/books.ts
+++ server/routes/books.ts
@@ -4,6 +4,7 @@ import { findBookById } from "../models/Book";
 var router = express.Router();
 
 router.get("/books/:slug", function (req, res, next) {
+  console.log(res.locals.user);
   res.render("book", {
     title: "Homepage",
     user: res.locals.user,
