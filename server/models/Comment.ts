23d51bc521e1843a711a70f9e5667825a5ff5bcd
1712092 <pbminh2511+PTUDW@gmail.com>
2020-06-03 10:47:22 +0700

    Add comment model

---
+++ server/models/Comment.ts
@@ -0,0 +1,26 @@
+import User, { DUMMY_USER } from "./User";
+import Chance from "chance";
+
+const chance = new Chance();
+
+interface Comment {
+    user: User;
+    content: string;
+    replies: Comment[];
+}
+
+function generate(n: number, depth: number): Comment[] {
+    const comments: Comment[] = [];
+    for (let i = 0; i < n; i++){
+        comments.push({
+            user: DUMMY_USER,
+            content: chance.sentence(),
+            replies: depth > 1 ? generate(n, depth - 1) : [],
+        });
+    };
+    return comments;
+}
+
+const DUMMY_COMMENTS: Comment[] = generate(5,2);
+export default Comment
+export { DUMMY_COMMENTS };
