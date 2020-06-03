4284878b7f915cbc8d53a6d1c73fe5d8ccc46c9f
1712092 <pbminh2511+PTUDW@gmail.com>
2020-06-03 07:55:01 +0700

    Add types/slick and fix few css errors

--- client/book_carousel.ts
+++ client/book_carousel.ts
@@ -1,7 +1,6 @@
 import $ from "jquery";
 import 'slick-carousel'
 function setup(){
-    let i = 0;
     const carouselWrappers = $('.book-carousel-wrapper')
     const slickObject = {
         infinite: true,
@@ -9,7 +8,7 @@ function setup(){
         slidesToScroll: 4,
         appendArrows: carouselWrappers[0],
         prevArrow: `
-        <div class="slick-button-wrapper">
+        <div class="slick-button-wrapper pr-10">
             <button type="button" class="slick-prev">
                 <svg width="18" height="36" viewBox="0 0 18 36" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M18 31.77L6.87449 18L18 4.23L14.5749 0L0 18L14.5749 36L18 31.77Z" fill="#F08804"/>
@@ -17,7 +16,7 @@ function setup(){
             </button>
         </div>`,
         nextArrow: `
-        <div class="slick-button-wrapper">
+        <div class="slick-button-wrapper pl-8">
             <button type="button" class="slick-next">
                 <svg width="18" height="36" viewBox="0 0 18 36" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 31.77L11.1255 18L0 4.23L3.4251 0L18 18L3.4251 36L0 31.77Z" fill="#F08804"/>
@@ -34,10 +33,13 @@ function setup(){
             },
         ]
     };
-    $('.book-carousel').on('init', function(event, slick) {
-        i+=1;
-        slickObject.appendArrows = carouselWrappers[i];
-    });
+    {
+        let i = 0;
+        $('.book-carousel').on('init', function(event, slick) {
+            i+=1;
+            slickObject.appendArrows = carouselWrappers[i];
+        });
+    }
     $('.book-carousel').slick(slickObject);
 }
 
