07f7710e0c6ec0fa014953983dca332d558968a7
1712092 <pbminh2511+PTUDW@gmail.com>
2020-06-15 20:34:07 +0700

    Move slick button into hbs and fix responsive for carousel

--- client/book_carousel.ts
+++ client/book_carousel.ts
@@ -1,45 +1,38 @@
 import $ from "jquery";
 import 'slick-carousel'
 function setup(){
-    const carouselWrappers = $('.book-carousel-wrapper')
     const slickObject = {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 4,
-        appendArrows: carouselWrappers[0],
-        prevArrow: `
-        <div class="slick-button-wrapper pr-10">
-            <button type="button" class="slick-prev">
-                <svg width="18" height="36" viewBox="0 0 18 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M18 31.77L6.87449 18L18 4.23L14.5749 0L0 18L14.5749 36L18 31.77Z" fill="#F08804"/>
-                </svg>
-            </button>
-        </div>`,
-        nextArrow: `
-        <div class="slick-button-wrapper pl-8">
-            <button type="button" class="slick-next">
-                <svg width="18" height="36" viewBox="0 0 18 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 31.77L11.1255 18L0 4.23L3.4251 0L18 18L3.4251 36L0 31.77Z" fill="#F08804"/>
-                </svg>
-            </button>
-        </div>`,
+        arrows: false,
         responsive: [
             {
-                breakpoint: 1279,
+                breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             },
         ]
     };
-    {
-        let i = 0;
-        $('.book-carousel').on('init', function(event, slick) {
-            i+=1;
-            slickObject.appendArrows = carouselWrappers[i];
+    $('.book-carousel').on('init', function(event, slick) {
+        const carousel = $(event.target);
+        carousel.parent().find('.slick-next').on('click', () => {
+            carousel.slick('slickNext');
+            
         });
-    }
+        carousel.parent().find('.slick-prev').on('click', () => {
+            carousel.slick('slickPrev');
+        });
+    });
     $('.book-carousel').slick(slickObject);
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   purge: false,
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        "2xs": "0.6rem",
+        "3xs": "0.5rem",
+        "4xs": "0.4rem",
+      },
+    },
   },
   variants: {},
   plugins: [],
