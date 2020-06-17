defc0f09309fc66d3ab87ac003871dfab03ffc9d
1712092 <pbminh2511+PTUDW@gmail.com>
2020-06-17 18:42:31 +0700

    Add trans when switch tab and testimonial

--- client/book_detail.ts
+++ client/book_detail.ts
@@ -1,12 +1,13 @@
 import $ from 'jquery';
 export default function setup(){
     const highlightClass = "book__detail-tab--highlight";
+    const contentHiddenClass = "book__detail-content--hidden";
     $(".book__detail-tabs a").on('click', (event) => {
         $(".book__detail-tabs a").removeClass(highlightClass);
         $(event.target).addClass(highlightClass);
 
         const tabName = event.target.dataset.name;
-        $(".book__detail-content > *").toggleClass('hidden');
+        $(".book__detail-content > *").toggleClass(contentHiddenClass);
     });
     $(".book__detail-tabs a").first().addClass(highlightClass);
 }
