ae3410044f7b9badb8718b2f999575211bd14f45
1712932 <abcdef6199@gmail.com>
2020-06-20 09:51:46 +0700

    Implement search page
    Implement sort select
    
    Add responsive
    
    Add responsive for filter
    
    Fix responsive for filter
    
    Fix responsive
    
    Fix pagination

--- server/server.ts
+++ server/server.ts
@@ -10,6 +10,7 @@ import authRouter from "./routes/auth";
 import catalogueRouter from "./routes/catalogue";
 import booksRouter from "./routes/books";
 import newsRouter from "./routes/news";
+import searchRouter from "./routes/search";
 
 import { parseAuth } from "./middlewares/auth";
 
@@ -40,6 +41,7 @@ app.use("/", authRouter);
 app.use("/", catalogueRouter);
 app.use("/", booksRouter);
 app.use("/news", newsRouter);
+app.use("/search", searchRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,18 @@ module.exports = {
   purge: false,
   theme: {
     extend: {
+      screens: {
+        xs: "515px"
+      },
       fontSize: {
         "2xs": "0.6rem",
         "3xs": "0.5rem",
         "4xs": "0.4rem"
       },
       minWidth: {
+        "1/6": "16.67%",
+        "1/5": "20%",
+        "1/4": "25%",
         "12": "3rem"
       }
     }
