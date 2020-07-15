efbe29377edb95ed1d09ccbce2b2dbc8c8c3d3fe
1712092 <pbminh2511+PTUDW@gmail.com>
2020-07-15 20:06:21 +0700

    Add basic auth with passportjs and bcrypt

--- server/migrations/1593397334609-SeedUserTable.ts
+++ server/migrations/1593397334609-SeedUserTable.ts
@@ -4,13 +4,13 @@ import { DUMMY_USERS } from "../entities/User";
 export class SeedUserTable1593397334609 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     for (let user of DUMMY_USERS) {
-      user.save();
+      await user.save();
     }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     for (let user of DUMMY_USERS) {
-      user.remove();
+      await user.remove();
     }
   }
 }
